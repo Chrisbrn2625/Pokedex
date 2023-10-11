239bf53dff88e6eff5e48a51336f0f02552cb601
Christophe BRUNO <christophebruno20@hotmail.com>
2023-10-11 12:23:53 +0200

    Quête 2 exercice Pokedex

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import PokemonCard from "./components/PokemonCards"
+
+
+
+function App() {
+
+
+  return (
+
+    <>
+      <div>
+        <PokemonCard />
+      </div>
+
+    </>
+  )
+}
+
+export default App
